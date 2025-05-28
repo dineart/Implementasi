f6cf691e96a6ab683428f0832db9b7f7449cb1f6
dineart <di2601na@gmail.com>
2025-05-28 15:31:56 +0800

    menambahkan definisi konstanta MAXSTACK

---
+++ c030324017stack.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#define MAXSTACK 100
+
+typedef int ItemType;
+
+typedef struct {
+    ItemType Item[MAXSTACK];
+    int Count;
+} Stack;
+
+void initializeStack(Stack *S) {
+    S->Count = 0;
+}
+
+int Full(Stack *S) {
+    return (S->Count == MAXSTACK);
+}
+
+int Empty(Stack *S) {
+    return (S->Count == 0);
+}
+
+void Pop(Stack *S, ItemType *x) {
+    if (S->Count==0)
+        cout<<"Stack masih kosong !"<<endl;
+    else {
+        --(S->Count);
+        *x = S->Item[S->Count];
+    }
+}
+
+void Push(ItemType x, Stack *S) {
+    if (S->Count==MAXSTACK)
+        cout << "Stack penuh! Data tidak dapat masuk!"<<endl;
+    else {
+        S->Item[S->Count]=x;
+        ++(S->Count);
+    }
+}
