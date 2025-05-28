95954cc28a43203abd6b75793b52eb5a2226a4fb
dineart <di2601na@gmail.com>
2025-05-28 15:33:54 +0800

    koreksi parameter dan logika

--- c030324017stack.cpp
+++ c030324017stack.cpp
@@ -14,12 +14,15 @@ void initializeStack(Stack *S) {
     S->Count = 0;
 }
 
-int Full(Stack *S) {
-    return (S->Count == MAXSTACK);
-}
+
 
 int Empty(Stack *S) {
     return (S->Count == 0);
+
+}
+
+int Full(Stack *S) {
+    return (S->Count == MAXSTACK);
 }
 
 void Pop(Stack *S, ItemType *x) {
